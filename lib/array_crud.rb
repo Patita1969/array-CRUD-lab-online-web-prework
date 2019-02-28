dda2a0c0ff34b6674731998b8851c5fa887b2da7
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-28 07:38:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("yuck")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -25,7 +25,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array(index_number)
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
